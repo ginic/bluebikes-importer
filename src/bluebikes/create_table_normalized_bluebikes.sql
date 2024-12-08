432f6f6d6e62ae6587a7a4b261c08db9cdd79782
Virginia Partridge <vcpartridge@gmail.com>
2024-12-07 23:27:52 -0500

    Updates to station overrides and normalization query

--- src/bluebikes/create_table_normalized_bluebikes.sql
+++ src/bluebikes/create_table_normalized_bluebikes.sql
@@ -51,14 +51,14 @@ SELECT
 	b.tripduration AS tripduration,
 	b.started_at AS started_at,
 	b.ended_at AS ended_at,
-	s1.raw_id AS start_id,
-	s1.name AS start_station_name,
-	s1.latitude AS start_lat,
-	s1.longitude AS start_lng,
-	s2.raw_id AS end_id,
-	s2.name AS end_station_name,
-	s2.latitude AS end_lat,
-	s2.longitude AS end_lng,
+	links1.normalized_id AS start_id,
+	links1.normalized_name AS start_station_name,
+	links1.normalized_latitude AS start_lat,
+	links1.normalized_longitude AS start_lng,
+	links2.normalized_id AS end_id,
+	links2.normalized_name AS end_station_name,
+	links2.normalized_latitude AS end_lat,
+	links2.normalized_longitude AS end_lng,
 	b.ride_id AS ride_id,
 	CASE WHEN (b.usertype = 'casual' OR b.usertype = 'Customer') THEN 'casual'
 		ELSE 'member' --'member' or 'Subscriber'
@@ -67,12 +67,10 @@ SELECT
 		WHEN b.rideable_type = 'electric_bike' THEN 'electric_bike'
 		ELSE 'classic_bike'
 		END ridable_type,
-	s1.geom_point AS start_point,
-	s2.geom_point AS end_point
+	links1.normalized_geom_point AS start_point,
+	links2.normalized_geom_point AS end_point
 FROM bluebikes b
 -- start station corrected
-JOIN station_links links1 ON links1.raw_id = b.start_id AND links1.raw_name = b.start_station_name --
-JOIN stations s1 ON s1.raw_id = links1.correct_id
+JOIN all_trips_stations links1 ON links1.station_id = b.start_id AND links1.station_name = b.start_station_name
 -- end station corrected
-JOIN station_links links2 ON links2.raw_id = b.end_id and links2.raw_name = b.end_station_name
-JOIN stations s2 on s2.raw_id = links2.correct_id;
+JOIN all_trips_stations links2 ON links2.station_id = b.end_id and links2.station_name = b.end_station_name;
